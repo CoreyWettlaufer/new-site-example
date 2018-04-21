e53c39a5cea67142e3a09b0ed7126225f053522c
Corey Wettlaufer <corey.wettlaufer@gmail.com>
2018-04-21 08:35:19 -0400

    header navigation

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
                   <h1>Logo Will Go Here</h1>
                </div>
 
-               <div id="header-social" class="col col-lg-8 col-md-8 col-sm-4 align-self-center d-flex justify-content-end flex-column">
+               <div id="header-social" class="col col-lg-8 col-md-8 col-sm-4 align-self-center d-flex justify-content-end flex-column align-items-end">
                   <div class="social-section">
                      <a href="" class="social-icon"><img src="<?php bloginfo('template_url'); ?>/img/icons/facebook-icon.png" alt="facebook icon"></a>
                      <a href="" class="social-icon"><img src="<?php bloginfo('template_url'); ?>/img/icons/twitter-icon.png" alt="twitter icon"></a>
